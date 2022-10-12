92d947b84a2872583a9dfe585abd383dd66abdc9
ChienPhV <phvchien@gmail.com>
2022-10-12 21:45:52 +0700

    upload

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import seedRouter from './routes/seedRoutes.js'
 import productRouter from './routes/productRoutes.js'
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import uploadRouter from './routes/uploadRoutes.js';
 
 //connect to DB
 dotenv.config();
@@ -33,6 +34,7 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/upload', uploadRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
