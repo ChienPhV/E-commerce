8319e53c45321741da3442dcab090fd87c1d8b86
Chien Pham <89862038+ChienPhV@users.noreply.github.com>
2022-11-04 16:23:41 +0700

    order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,356 +1,182 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useReducer } from 'react';
-import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
-import { Helmet } from 'react-helmet-async';
-import { useNavigate, useParams } from 'react-router-dom';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { Store } from '../Store';
-import { getError } from '../utils';
-import { toast } from 'react-toastify';
-// import StripeCheckout from 'react-stripe-checkout';
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+import User from '../models/userModel.js';
+import Product from '../models/productModel.js';
+import { isAuth, isAdmin, mailgun, payOrderEmailTemplate } from '../utils.js';
 
+const orderRouter = express.Router();
 
+orderRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find().populate('user', 'name');
+    res.send(orders);
+  })
+);
 
-// const KEY = process.env.REACT_APP_STRIPE;
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'FETCH_REQUEST':
-            return { ...state, loading: true, error: '' };
-        case 'FETCH_SUCCESS':
-            return { ...state, loading: false, order: action.payload, error: '' };
-        case 'FETCH_FAIL':
-            return { ...state, loading: false, error: action.payload };
-        case 'PAY_REQUEST':
-            return { ...state, loadingPay: true };
-        case 'PAY_SUCCESS':
-            return { ...state, loadingPay: false, successPay: true };
-        case 'PAY_FAIL':
-            return { ...state, loadingPay: false };
-        case 'PAY_RESET':
-            return { ...state, loadingPay: false, successPay: false };
-
-        case 'DELIVER_REQUEST':
-            return { ...state, loadingDeliver: true };
-        case 'DELIVER_SUCCESS':
-            return { ...state, loadingDeliver: false, successDeliver: true };
-        case 'DELIVER_FAIL':
-            return { ...state, loadingDeliver: false };
-        case 'DELIVER_RESET':
-            return {
-                ...state,
-                loadingDeliver: false,
-                successDeliver: false,
-            };
-        default:
-            return state;
-    }
-}
-export default function OrderScreen() {
-
-    const { state } = useContext(Store);
-    const { userInfo } = state;
-
-    const params = useParams();
-    const { id: orderId } = params;
-    const navigate = useNavigate();
-
-    const [
-        {
-            loading,
-            error,
-            order,
-            successPay,
-            loadingPay,
-            loadingDeliver,
-            successDeliver,
-        },
-        dispatch,
-    ] = useReducer(reducer, {
-        loading: true,
-        order: {},
-        error: '',
-        successPay: false,
-        loadingPay: false,
+orderRouter.post(
+  '/',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const newOrder = new Order({
+      orderItems: req.body.orderItems.map((x) => ({ ...x, product: x._id })),
+      shippingAddress: req.body.shippingAddress,
+      paymentMethod: req.body.paymentMethod,
+      itemsPrice: req.body.itemsPrice,
+      shippingPrice: req.body.shippingPrice,
+      taxPrice: req.body.taxPrice,
+      totalPrice: req.body.totalPrice,
+      user: req.user._id,
     });
 
-    const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+    const order = await newOrder.save();
+    res.status(201).send({ message: 'New Order Created', order });
+  })
+);
 
-    function createOrder(data, actions) {
-        return actions.order
-            .create({
-                purchase_units: [
-                    {
-                        amount: { value: order.totalPrice },
-                    },
-                ],
-            })
-            .then((orderID) => {
-                return orderID;
-            });
-    }
-
-    function onApprove(data, actions) {
-        return actions.order.capture().then(async function (details) {
-            try {
-                dispatch({ type: 'PAY_REQUEST' });
-                const { data } = await axios.put(
-                    `/api/orders/${order._id}/pay`,
-                    details,
-                    {
-                        headers: { authorization: `Bearer ${userInfo.token}` },
-                    }
-                );
-                dispatch({ type: 'PAY_SUCCESS', payload: data });
-                toast.success('Order is paid');
-            } catch (err) {
-                dispatch({ type: 'PAY_FAIL', payload: getError(err) });
-                toast.error(getError(err));
-            }
-        });
-    }
-    function onError(err) {
-        toast.error(getError(err));
-    }
-
-    useEffect(() => {
-        const fetchOrder = async () => {
-            try {
-                dispatch({ type: 'FETCH_REQUEST' });
-                const { data } = await axios.get(`/api/orders/${orderId}`, {
-                    headers: { authorization: `Bearer ${userInfo.token}` },
-                });
-                dispatch({ type: 'FETCH_SUCCESS', payload: data });
-            } catch (err) {
-                dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
-            }
-        };
-
-        if (!userInfo) {
-            return navigate('/login');
-        }
-        if (
-            !order._id ||
-            successPay ||
-            successDeliver ||
-            (order._id && order._id !== orderId)
-        ) {
-            fetchOrder();
-            if (successPay) {
-                dispatch({ type: 'PAY_RESET' });
-            }
-            if (successDeliver) {
-                dispatch({ type: 'DELIVER_RESET' });
-            }
-        } else {
-            const loadPaypalScript = async () => {
-                const { data: clientId } = await axios.get('/api/keys/paypal', {
-                    headers: { authorization: `Bearer ${userInfo.token}` },
-                });
-                paypalDispatch({
-                    type: 'resetOptions',
-                    value: {
-                        'client-id': clientId,
-                        currency: 'USD',
-                    },
-                });
-                paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
-            };
-            loadPaypalScript();
-        }
-    }, [
-        order,
-        userInfo,
-        orderId,
-        navigate,
-        paypalDispatch,
-        successPay,
-        successDeliver,
+orderRouter.get(
+  '/summary',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.aggregate([
+      {
+        $group: {
+          _id: null,
+          numOrders: { $sum: 1 },
+          totalSales: { $sum: '$totalPrice' },
+        },
+      },
     ]);
+    const users = await User.aggregate([
+      {
+        $group: {
+          _id: null,
+          numUsers: { $sum: 1 },
+        },
+      },
+    ]);
+    const dailyOrders = await Order.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+          orders: { $sum: 1 },
+          sales: { $sum: '$totalPrice' },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+    const productCategories = await Product.aggregate([
+      {
+        $group: {
+          _id: '$category',
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    res.send({ users, orders, dailyOrders, productCategories });
+  })
+);
 
-    async function deliverOrderHandler() {
-        try {
-            dispatch({ type: 'DELIVER_REQUEST' });
-            const { data } = await axios.put(
-                `/api/orders/${order._id}/deliver`,
-                {},
-                {
-                    headers: { authorization: `Bearer ${userInfo.token}` },
-                }
-            );
-            dispatch({ type: 'DELIVER_SUCCESS', payload: data });
-            toast.success('Order is delivered');
-        } catch (err) {
-            toast.error(getError(err));
-            dispatch({ type: 'DELIVER_FAIL' });
-        }
+orderRouter.get(
+  '/mine',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+
+orderRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
     }
+  })
+);
 
+orderRouter.put(
+  '/:id/deliver',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+      await order.save();
+      res.send({ message: 'Order Delivered' });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
 
-    return loading ? (
-        <LoadingBox></LoadingBox>
-    ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
-    ) : (
-        <div>
-            <Helmet>
-                <title>Order {orderId}</title>
-            </Helmet>
-            <h1 className="my-3">Order</h1>
-            <Row>
-                <Col md={8}>
-                    <Card className="mb-3">
-                        <Card.Body>
-                            <Card.Title>Shipping</Card.Title>
-                            <Card.Text>
-                                <strong>Name:</strong> {order.shippingAddress.fullName} <br />
-                                <strong>Address: </strong> {order.shippingAddress.address},
-                                {order.shippingAddress.city}, {order.shippingAddress.postalCode}
-                                ,{order.shippingAddress.country}
-                                &nbsp;
-                                {order.shippingAddress.location &&
-                                    order.shippingAddress.location.lat && (
-                                        <a
-                                            target="_new"
-                                            href={`https://maps.google.com?q=${order.shippingAddress.location.lat},${order.shippingAddress.location.lng}`}
-                                        >
-                                            Show On Map
-                                        </a>
-                                    )}
-                            </Card.Text>
-                            {order.isDelivered ? (
-                                <MessageBox variant="success">
-                                    Delivered at {order.deliveredAt}
-                                </MessageBox>
-                            ) : (
-                                <MessageBox variant="danger">Not Delivered</MessageBox>
-                            )}
-                        </Card.Body>
-                    </Card>
-                    <Card className="mb-3">
-                        <Card.Body>
-                            <Card.Title>Payment</Card.Title>
-                            <Card.Text>
-                                <strong>Method:</strong> {order.paymentMethod}
-                            </Card.Text>
-                            {order.isPaid ? (
-                                <MessageBox variant="success">
-                                    Paid at {order.paidAt}
-                                </MessageBox>
-                            ) : (
-                                <MessageBox variant="danger">Not Paid</MessageBox>
-                            )}
-                        </Card.Body>
-                    </Card>
-
-                    <Card className="mb-3">
-                        <Card.Body>
-                            <Card.Title>Items</Card.Title>
-                            <ListGroup variant="flush">
-                                {order.orderItems.map((item) => (
-                                    <ListGroup.Item key={item._id}>
-                                        <Row className="align-items-center">
-                                            <Col md={6}>
-                                                <img
-                                                    src={item.image}
-                                                    alt={item.name}
-                                                    className="img-fluid rounded img-thumbnail"
-                                                ></img>{' '}
-                                                <Link to={`/product/${item.slug}`}>{item.name}</Link>
-                                            </Col>
-                                            <Col md={3}>
-                                                <span>{item.quantity}</span>
-                                            </Col>
-                                            <Col md={3}>${item.price}</Col>
-                                        </Row>
-                                    </ListGroup.Item>
-                                ))}
-                            </ListGroup>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={4}>
-                    <Card className="mb-3">
-                        <Card.Body>
-                            <Card.Title>Order Summary</Card.Title>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Items</Col>
-                                        <Col>${order.itemsPrice.toFixed(2)}</Col>
-                                    </Row>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Shipping</Col>
-                                        <Col>${order.shippingPrice.toFixed(2)}</Col>
-                                    </Row>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Tax</Col>
-                                        <Col>${order.taxPrice.toFixed(2)}</Col>
-                                    </Row>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>
-                                            <strong> Order Total</strong>
-                                        </Col>
-                                        <Col>
-                                            <strong>${order.totalPrice.toFixed(2)}</strong>
-                                        </Col>
-                                    </Row>
-                                </ListGroup.Item>
-                                {!order.isPaid && (
-                                    <><ListGroup.Item>
-                                        {isPending ? (
-                                            <LoadingBox />
-                                        ) : (
-                                            <div>
-                                                <PayPalButtons
-                                                    createOrder={createOrder}
-                                                    onApprove={onApprove}
-                                                    onError={onError}
-                                                ></PayPalButtons>
-
-                                            </div>
-                                        )}
-                                        {/* <StripeCheckout
-                                            name="Vietnam Woodlands"
-                                            billingAddress
-                                            shippingAddress
-                                            description={`Your total is $${order.totalPrice.toFixed(2)}`}
-                                            amount={order.totalPrice.toFixed(2) * 100}
-                                            stripeKey={KEY}
-                                        >
-                                            <Button type="button" style={{ width: 290 }} onClick={onApprove}>STRIPE CHECKOUT</Button>
-                                        </StripeCheckout> */}
-                                        {loadingPay && <LoadingBox></LoadingBox>}
-                                    </ListGroup.Item></>
-                                )}
-                                {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
-                                    <ListGroup.Item>
-                                        {loadingDeliver && <LoadingBox></LoadingBox>}
-                                        <div className="d-grid">
-                                            <Button type="button" onClick={deliverOrderHandler}>
-                                                Deliver Order
-                                            </Button>
-                                        </div>
-                                    </ListGroup.Item>
-                                )}
-                            </ListGroup>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'email name'
     );
-}
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+
+      const updatedOrder = await order.save();
+      mailgun()
+        .messages()
+        .send(
+          {
+            from: 'Vietnam Woodlands <phvchien@vietnamwoodlands.com>',
+            to: `${order.user.name} <${order.user.email}>`,
+            subject: 'Thanks for your purchasing!',
+            html: payOrderEmailTemplate(order),
+
+          },
+          (error, body) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(body);
+            }
+          }
+        );
+
+      res.send({ message: 'Order Paid', order: updatedOrder });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
+orderRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      await order.remove();
+      res.send({ message: 'Order Deleted' });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
+export default orderRouter;
